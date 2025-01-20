3f38a750721a6bb01346c0895d9ccf072a442a44
droidrdee <deepak.yourdomain@gmail.com>
2025-01-20 23:48:04 +0530

    Initialized e commerce project and setuped product, accounts and template things

---
+++ ecomm/ecomm/accounts/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+
+
+
+urlpatterns = [
+    path('login/', views.login_page, name="login_page"),
+    path('register/', views.register_page, name ="register_page")
+]
